9b8d8e5c92945c0443162a0b938323164a9d89b5
Alex <alexpit63@gmail.com>
2018-08-05 21:12:02 +0300

    Добавьте файлы проекта.

---
+++ Csharp_LabRab1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Csharp_LabRab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Hello World!");
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            finally
+            {
+                Console.Write("Press <Enter>");
+                Console.ReadLine();
+            }
+        }
+    }
+}
